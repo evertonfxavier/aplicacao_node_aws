4dee8f16da1ca08cb9c7fa4d177a696b8552ea7e
Everton Xavier <evertonfxavier@gmail.com>
2023-01-28 19:12:00 -0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is up" });
+  return res.json({ message: "Atualizou mesmo 😀" });
 });
 
 app.listen(3333);
